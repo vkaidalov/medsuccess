06c223abaedcf2e429b9c601e1a0ff160028d5c7
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-01-09 14:08:14 +0200

    fixed login problem on revisiting the app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
 
 Vue.config.productionTip = false;
+
+const token = localStorage.getItem('token');
+if (token) {
+  axios.defaults.headers.common['Authorization'] = "Token " + token;
+}
 
 new Vue({
     router,
