d880c298c55a5a96dd367cf8ba1595faa97b744c
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-01-15 20:34:54 +0200

    added uploading/receiving routes and measure simulation for esp8266

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -3,11 +3,28 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <Ticker.h>
+#include <time.h>
 
 const char* ssid = "VadimK";
 const char* password = "afynjvfc";
 
 ESP8266WebServer server(80);
+
+bool isScheduleLoaded = false;
+
+// Variables below have sense if (isScheduleLoaded == true).
+int min_t  = -1;
+int max_t  = -1;
+int min_rh = -1;
+int max_rh = -1;
+int scheduleLoadedTime = -1;
+
+const int DOSES_SIZE = 200;
+int doses_len = 0;
+int doseIds[DOSES_SIZE];    // dose_ids[i] is id of a dose in backend database.
+int doseTimers[DOSES_SIZE]; // dose_timers[i] is how much time left before consuming.
+bool isDoseConsumed[DOSES_SIZE];
+bool isDoseAlerted[DOSES_SIZE];
 
 const int wifiLedPin = 2;
 // Declared explicitly because only for wifiLedPin LOW is HIGH and vice versa.
@@ -42,20 +59,19 @@ String arrToStr(String label, int n, int *arr) {
   return res;
 }
 
-void dangerousBlink() {
+void alertBlink(int localDelay, int repeats) {
   int currentState = digitalRead(wifiLedPin);
   unsigned long cur_millis;
-  unsigned long local_delay = 300;
   
-  for (int i = 0; i < 10; ++i) {
+  for (int i = 0; i < repeats; ++i) {
     currentState = currentState == HIGH ? LOW : HIGH;
     digitalWrite(wifiLedPin, currentState);
     cur_millis = millis();
-    while (millis() < cur_millis + local_delay) {}
+    while (millis() < cur_millis + localDelay) {}
     currentState = currentState == HIGH ? LOW : HIGH;
     digitalWrite(wifiLedPin, currentState);
     cur_millis = millis();
-    while (millis() < cur_millis + local_delay) {}
+    while (millis() < cur_millis + localDelay) {}
   }
 }
 
@@ -63,11 +79,23 @@ Ticker measurer;
 
 void measure() {
   noInterrupts();
-  int cur_temp = random(230, 255);
-  int cur_rh = random(550, 675);
+  int cur_time = time(NULL);
+  for (int i = 0; i < doses_len; ++i) {
+    if (!isDoseAlerted[i] && cur_time - scheduleLoadedTime > doseTimers[i]) {
+      alertBlink(300, 10);
+      isDoseAlerted[i] = true;
+      isDoseConsumed[i] = random(0, 5) > 0; // 20% of fail simulation
+      break;
+    }
+  }
   
-  if (cur_temp > 250 || cur_rh > 650) {
-    dangerousBlink();
+  int cur_temp = random(230, 255);
+  int cur_rh = random(575, 626);
+  
+  if (isScheduleLoaded && 
+      (cur_temp < min_t || cur_temp > max_t || cur_rh < min_rh || cur_rh > max_rh)
+  ) {
+    alertBlink(100, 30);
   }
   
   addRecord(cur_temp, cur_rh);
@@ -95,11 +123,107 @@ void handleSensors() {
   interrupts();
 }
 
+void handleSchedule() {
+  noInterrupts();
+  digitalWrite(wifiLedPin, wifiLedOn);
+
+  Serial.println("handleSchedule");
+  int startIndex = -1, endIndex = -1;
+  String reqBody = server.arg("plain");
+
+  Serial.println(reqBody);
+  int cnt = 0;
+  for (int i = 0; i < reqBody.length(); ++i) {
+    if (reqBody[i] == '\"') {
+      ++cnt;
+    }
+    if (startIndex == -1 && cnt == 3) {
+      startIndex = i + 1;
+    }
+    else if (cnt == 4) {
+      endIndex = i;
+      break;
+    }
+  }
+  Serial.println(cnt);
+  Serial.println(startIndex);
+  Serial.println(endIndex);
+
+  int curIndex = startIndex, prevIndex = startIndex;
+  while(reqBody[curIndex] != ' ') ++curIndex;
+  doses_len = reqBody.substring(prevIndex, curIndex).toInt();
+  Serial.println("doses_len: " + doses_len);
+
+  for (int i = 0; i < doses_len; ++i) {
+    ++curIndex; // skipping the last ' '
+    prevIndex = curIndex;
+    while(reqBody[curIndex] != ' ') ++curIndex;
+    doseIds[i] = reqBody.substring(prevIndex, curIndex).toInt();
+    Serial.print(i + ": " + doseIds[i]);
+    ++curIndex; // skipping the last ' '
+    prevIndex = curIndex;
+    while(reqBody[curIndex] != ' ') ++curIndex;
+    doseTimers[i] = reqBody.substring(prevIndex, curIndex).toInt();
+    Serial.println(doseTimers[i]);
+  }
+
+  ++curIndex; // skipping the last ' '
+  prevIndex = curIndex;
+  while(reqBody[curIndex] != ' ') ++curIndex;
+  min_t = reqBody.substring(prevIndex, curIndex).toInt();
+  Serial.println("min_t: " + min_t);
+
+  ++curIndex; // skipping the last ' '
+  prevIndex = curIndex;
+  while(reqBody[curIndex] != ' ') ++curIndex;
+  max_t = reqBody.substring(prevIndex, curIndex).toInt();
+  Serial.println("max_t: " + max_t);
+
+  ++curIndex; // skipping the last ' '
+  prevIndex = curIndex;
+  while(reqBody[curIndex] != ' ') ++curIndex;
+  min_rh = reqBody.substring(prevIndex, curIndex).toInt();
+  Serial.println("min_rh: " + min_rh);
+
+  ++curIndex; // skipping the last ' '
+  prevIndex = curIndex;
+  max_rh = reqBody.substring(prevIndex, endIndex).toInt();
+  Serial.println("max_rh: " + max_rh);
+
+  for (int i = 0; i < doses_len; ++i) {
+    isDoseConsumed[i] = isDoseAlerted[i] = false;
+  }
+
+  isScheduleLoaded = true;
+  scheduleLoadedTime = time(NULL);
+
+  server.send(201, "text/plain", String(doses_len) + "," + String(min_t) + "," + String(max_t) + "," + String(min_rh) + "," + String(max_rh));
+  
+  digitalWrite(wifiLedPin, wifiLedOff);
+  interrupts();
+}
+
+void handleConsumes() {
+  noInterrupts();
+  digitalWrite(wifiLedPin, wifiLedOn);
+  String body = "{";
+  body += "\"data\":\"";
+  int cnt = 0;
+  String tmp = "";
+  for (int i = 0; i < doses_len; ++i) {
+    if (isDoseAlerted[i]) {
+      cnt++;
+      tmp += String(doseIds[i]) + " " + (isDoseConsumed[i] ? "1" : "0") + " ";
+    }
+  }
+  body += String(cnt) + " " + tmp + "\"";
+  body += "}";
+  server.send(200, "application/json", body);
+  digitalWrite(wifiLedPin, wifiLedOff);
+  interrupts();
+}
+
 void handleNotFound() {
-//  while (mutex) {
-//    // wait
-//  }
-//  ++mutex;
   noInterrupts();
   digitalWrite(wifiLedPin, wifiLedOn);
   String message = "File Not Found\n\n";
@@ -126,11 +250,11 @@ void handleNotFound() {
   
   server.send(404, "text/plain", message);
   digitalWrite(wifiLedPin, wifiLedOff);
-  interrupts();
-  //--mutex;
+    interrupts();
 }
 
 void setup(void) {
+  Serial.begin(115200);
   randomSeed(analogRead(0));
   
   Serial.println("setup begin");
@@ -142,7 +266,7 @@ void setup(void) {
   
   pinMode(wifiLedPin, OUTPUT);
   digitalWrite(wifiLedPin, wifiLedOff);
-  Serial.begin(115200);
+  
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -166,6 +290,10 @@ void setup(void) {
 
   server.on("/sensors", handleSensors);
 
+  server.on("/schedule", handleSchedule);
+
+  server.on("/consumes", handleConsumes);
+
   server.on("/inline", []() {
     server.send(200, "text/plain", "this works as well");
   });
@@ -175,7 +303,7 @@ void setup(void) {
   server.begin();
   Serial.println("HTTP server started");
 
-  measurer.attach(15, measure);
+  measurer.attach(60, measure);
   Serial.println("Measurer ticker started");
 }
 
